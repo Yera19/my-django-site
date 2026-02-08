7e3e6d25edf3aeb011b1479e383b009bdfc0c8cf
Yera19 <mrdesh7@gmail.com>
2026-02-09 03:52:28 +0500

    Update settings.py

--- myshop/settings.py
+++ myshop/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['yera-techshop.onrender.com', '127.0.0.1', 'localhost']
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,5 +120,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 
