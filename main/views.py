dc9f9c97830ffee4a243dcd1163190d621d80f4d
Yera19 <mrdesh7@gmail.com>
2026-02-19 15:45:41 +0500

    Update views.py

--- main/views.py
+++ main/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from .models import Product
+from django.http import JsonResponse
+from .models import Product, OrderRequest
+import json
+from django.views.decorators.csrf import csrf_exempt
 
 def index(request):
     category_name = request.GET.get('category')
@@ -15,12 +18,26 @@ def index(request):
     })
 
 def checkout(request):
-    return render(request, 'main/index.html')
+    # Открывает твой файл checkout.html
+    return render(request, 'main/checkout.html')
 
 def about(request):
-    return render(request, 'main/index.html')
+    # Открывает твой файл about.html (контакты)
+    return render(request, 'main/about.html')
 
+@csrf_exempt
 def create_order(request):
-    # Эта функция нужна, чтобы работала отправка заказа
-    return render(request, 'main/index.html')
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        # Создаем запись в базе данных
+        OrderRequest.objects.create(
+            client_name=data.get('name'),
+            phone=data.get('phone', 'Не указан'),
+            product_list=data.get('products'),
+            total_price=data.get('total'),
+            method=data.get('method'),
+            term=int(data.get('term', 0))
+        )
+        return JsonResponse({'status': 'ok'})
+    return JsonResponse({'status': 'error'}, status=400)
     
